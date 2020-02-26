6fad422a0d1bf43d88649c17ffcd23cf5749ba48
Solar Flare <join200022@gmail.com>
2020-02-26 22:00:12 +0300

    Initial commit. Created base for the Core.

---
+++ SolPM.Core/App.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.ViewModels;
+using MvvmCross.IoC;
+using SolPM.Core.ViewModels;
+
+namespace SolPM.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<HomeViewModel>();
+        }
+    }
+}
---
+++ SolPM.Core/ViewModels/HomeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MvvmCross.Commands;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+
+namespace SolPM.Core.ViewModels
+{
+    class HomeViewModel : MvxViewModel
+    {
+        private readonly IMvxNavigationService _navigationService;
+
+        public HomeViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            // Commands
+        }
+
+        #region Commands
+
+        #endregion
+
+        #region Command Functions
+
+        #endregion
+    }
+}
