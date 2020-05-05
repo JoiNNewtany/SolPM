764fc7d3fda306f4f8d2053620101d8976ab1bb0
Solar Flare <join200022@gmail.com>
2020-05-05 05:08:48 +0300

    Add dialogs
    Now there are graphical elements that allow
    the View to send a signal back to the ViewModel
    after an interaction. They are unused at the moment.

---
+++ SolPM.WPF/Dialogs/YesNoDialog.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+
+namespace SolPM.WPF.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for YesNoDialog.xaml
+    /// </summary>
+    public partial class YesNoDialog : Grid
+    {
+        public YesNoDialog()
+        {
+            InitializeComponent();
+        }
+
+        public string Message
+        {
+            get { return MessageBlock.Text; }
+            set { MessageBlock.Text = value; }
+        }
+    }
+}
