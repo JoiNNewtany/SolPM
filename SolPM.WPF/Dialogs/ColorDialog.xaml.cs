3bbab7f47b1b8372581992b8a81d051bf9bb2889
Solar Flare <join200022@gmail.com>
2020-05-19 01:36:51 +0300

    Add ColorDialog

---
+++ SolPM.WPF/Dialogs/ColorDialog.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SolPM.WPF.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for YesNoDialog.xaml
+    /// </summary>
+    public partial class ColorDialog : Grid
+    {
+        public ColorDialog()
+        {
+            InitializeComponent();
+        }
+
+        public Color SelectedColor
+        {
+            get { return ColorPicker.Color; }
+        }
+    }
+}
